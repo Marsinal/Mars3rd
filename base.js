2e8d6102070dd7596ede991280022dd3448a2db9
Marsinal <98711430+Marsinal@users.noreply.github.com>
2024-10-06 11:48:11 +0300

    Update base.js
    Sides on cube

--- base.js
+++ base.js
@@ -27,9 +27,16 @@ console.log(map)*/
     }
 }*/
 
-var perspective = 500;
-var camera = [10000,10000,10000]
-const Cube = [[[2, 3, 5, '#ffffff'],[3, 4, 5, '#ffffff'],[4, 5, 5, '#ffffff'],[5, 2, 5, '#ffffff'],[6, 7, 5, '#ffffff'],[7, 8, 5, '#ffffff'],[8, 9, 5, '#ffffff'],[9, 6, 5, '#ffffff'], [2, 6, 5, '#ffffff'],[3, 7, 5, '#ffffff'],[4, 8, 5, '#ffffff'],[5, 9, 5, '#ffffff']],[[2,3,4,5,[0,0,0],"grass_block"],[3,4,4,5,[0,0,0],"grass_block"],[2,3,4,5,[0,0,0],"grass_block"],[2,3,4,5,[0,0,0],"grass_block"],[2,3,6,7,[0,0,0],"grass"],[2,3,4,5,[0,0,0],"glass_block"]],[100, 100, 100],[-100, 100, 100],[-100, -100, 100],[100, -100, 100],[100, 100, -100],[-100, 100, -100],[-100, -100, -100],[100, -100, -100]];
+var perspective = 1000;
+var camera = [0,0,10000]
+//const Cube = [[[2, 3, 5, '#ffffff'],[3, 4, 5, '#ffffff'],[4, 5, 5, '#ffffff'],[5, 2, 5, '#ffffff'],[6, 7, 5, '#ffffff'],[7, 8, 5, '#ffffff'],[8, 9, 5, '#ffffff'],[9, 6, 5, '#ffffff'], [2, 6, 5, '#ffffff'],[3, 7, 5, '#ffffff'],[4, 8, 5, '#ffffff'],[5, 9, 5, '#ffffff']],[[2,3,4,5,[0,0,0],"grass_block"],[3,4,4,5,[0,0,0],"grass_block"],[2,3,4,5,[0,0,0],"grass_block"],[2,3,4,5,[0,0,0],"grass_block"],[2,3,6,7,[0,0,0],"grass"],[2,3,4,5,[0,0,0],"glass_block"]],[100, 100, 100],[-100, 100, 100],[-100, -100, 100],[100, -100, 100],[100, 100, -100],[-100, 100, -100],[-100, -100, -100],[100, -100, -100]];
+/*
+for (let i = 0; i != Cube[0].length; i++) {
+    doLine(Cube[Cube[0][i][0]], Cube[Cube[0][i][1]], [getCenterX(),getCenterY()], Cube[0][i][2], Cube[0][i][3]);
+}*/
+const Cube = [[[[1,2,3,4],"#ffffff"],[[6,5,8,7],"#808080"],[[8,7,3,4],"#ff8080"],[[5,6,2,1],"#ff80ff"],[[2,6,7,3],"#405060"],[[5,1,4,8],"#40f560"]],[-100, 100, 100],[100, 100, 100],[100, -100, 100],[-100, -100, 100],[-100, 100, -100],[100, 100, -100],[100, -100, -100],[-100, -100, -100]]
+var centerSide = [0,0,0];
+var renderList = [];
 
 dis = new Display("display", [100,100], "#1a1a1a");
 displayAtWorkSet(dis);
@@ -39,14 +46,30 @@ dis.updateScale([window.innerWidth,window.innerHeight]);
 function step() {
     setTimeout(function() {
         dis.updateScale([window.innerWidth,window.innerHeight]);
-        dis.updateScreen()
+        dis.updateScreen();
         //doPixel([i,i], "#FFFFFF");
         //doSquare([100,100], "#FFFFFF", [512,512])
-        doRotate([0.1,0.1,0.1],Cube.slice(2,Cube.length));
+        doRotate([0.2,0.1,0.1],Cube.slice(1,Cube.length));
         for (let i = 0; i != Cube[0].length; i++) {
-            doLine(Cube[Cube[0][i][0]], Cube[Cube[0][i][1]], [getCenterX(),getCenterY()], Cube[0][i][2], Cube[0][i][3]);
+            centerSide = [0,0,0]
+            for (let x = 0; x != Cube[0][i][0].length; x++) {
+                centerSide[0] += Cube[Cube[0][i][0][x]][0];
+                centerSide[1] += Cube[Cube[0][i][0][x]][1];
+                centerSide[2] += Cube[Cube[0][i][0][x]][2];
+            }
+            centerSide[0] /= Cube[0][i][0].length;
+            centerSide[1] /= Cube[0][i][0].length;
+            centerSide[2] /= Cube[0][i][0].length;
+            renderList.push([Math.sqrt(Math.pow((camera[0]-centerSide[0]),2)+Math.pow((camera[1]-centerSide[1]),2)+Math.pow((camera[2]-centerSide[2]),2)),[Cube[Cube[0][i][0][0]],Cube[Cube[0][i][0][1]],Cube[Cube[0][i][0][2]],Cube[Cube[0][i][0][3]]],Cube[0][i][1]]);
+            //doFillNgon([Cube[Cube[0][i][0][0]],Cube[Cube[0][i][0][1]],Cube[Cube[0][i][0][2]],Cube[Cube[0][i][0][3]]], [getCenterX(),getCenterY()], Cube[0][i][1])
         }
-        fps[1]++
+        //console.log(renderList)
+        renderList = bubbleSort(renderList);
+        for (let i = 0; i != renderList.length; i++) {
+            doFillNgon(renderList[i][1], [getCenterX(),getCenterY()], renderList[i][2])
+        }
+        renderList = [];
+        fps[1]++;
         clearTimeout(step);
         step();
     }, 1000 / fps[0]);
